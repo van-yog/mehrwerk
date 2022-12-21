633600d2051b0e95147950f148fde9987df42918
Ivan <ikharchenko2008@gmail.com>
2022-12-21 13:02:08 +0100

    add hook for fetch shops

---
+++ src/hooks/useShop.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router';
+import axios from 'axios';
+import { api } from '../assets/api/api';
+
+const useShop = () => {
+  const [shop, setShop] = useState();
+  const [error, setError] = useState();
+  const location = useLocation();
+  console.log('location', location);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { url } = api.shops;
+
+      const shopId = location.pathname.slice(7);
+      const shopUrl = `${url}/${shopId}`;
+      console.log('shopId', shopId);
+      try {
+        const res = await axios.get(shopUrl, {
+          headers: {
+            'X-API-KEY': process.env.REACT_APP_X_API_KEY,
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+          },
+        });
+
+        setShop(res.data);
+        console.log('shops:', res.data);
+      } catch (e) {
+        console.log(e);
+        setShop(null);
+        setError(e);
+      }
+    };
+    fetchData();
+  }, []);
+  return { shop, error };
+};
+
+export default useShop;
