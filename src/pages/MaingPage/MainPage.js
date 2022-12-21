3e5b8623bf185f419f17bf5eb761498cf85fecb3
Ivan <ikharchenko2008@gmail.com>
2022-12-21 13:55:41 +0100

    app styles to main page

--- src/pages/MaingPage/MainPage.js
+++ src/pages/MaingPage/MainPage.js
@@ -3,11 +3,20 @@ import Hero from '../../components/Hero';
 
 const MainPage = () => {
   return (
-    <div>
+    <div className="main_page">
       <Hero
         title="Welcome to the main page"
         text="Below is a short video presentation that will introduce you to me and my application."
       />
+      <div className="main_page__link_wrapper">
+        <a
+          className="main_page__link"
+          href="https://www.loom.com/share/2b45bb3d8f3942098ff3bed3bf151bfc"
+          rel="noopener noreferrer"
+        >
+          https://www.loom.com/share/2b45bb3d8f3942098ff3bed3bf151bfc
+        </a>
+      </div>
     </div>
   );
 };
